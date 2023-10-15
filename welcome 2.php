2dcd109a0b9cee42d2362bef3a43b491a0b3d85b
EDWIN KAFUI GLIGAH <112875184+Forin2001@users.noreply.github.com>
2023-10-15 05:22:51 -0700

    Add files via upload

---
+++ welcome 2.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f2f2f2;
+            margin: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+
+        .welcome-container {
+            text-align: center;
+            padding: 20px;
+        }
+
+        .welcome-message {
+            font-size: 24px;
+            margin-bottom: 20px;
+        }
+    </style>
+</head>
+<body>
+    <div class="welcome-container">
+        <p class="welcome-message">Welcome to our website!</p>
+        
+    </div>
+</body>
+</html>
